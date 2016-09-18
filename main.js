2e20025498ecb5b6192678a7a21705d0baab65f9
xiongguoliang <513724462@qq.com>
2016-09-18 18:46:11 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2016/8/28.
+ */
+
+var fs = require("fs");
+
+fs.readFile('input.txt', function (err, data) {
+    if (err) return console.error(err);
+    console.log(data.toString());
+});
+
+console.log("程序执行结束!");
